154d55bb83803f7effd79caf38fd7f9f1676c100
Madhuritha_alle <63335924+madhuritha22@users.noreply.github.com>
2025-06-12 11:21:34 +0530

    Update NotificationPopup.java

--- app/src/cc/arduino/view/NotificationPopup.java
+++ app/src/cc/arduino/view/NotificationPopup.java
@@ -20,7 +20,7 @@
  * templates or use macros or inline functions from this file, or you compile
  * this file and link it with other files to produce an executable, this
  * file does not by itself cause the resulting executable to be covered by
- * the GNU General Public License.  This exception does not however
+ * the GNU General Public License. This exception does not however
  * invalidate any other reasons why the executable file might be covered by
  * the GNU General Public License.
  *
@@ -31,29 +31,18 @@ package cc.arduino.view;
 
 import static processing.app.Theme.scale;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.Image;
-import java.awt.Point;
+import java.awt.*;
 import java.awt.event.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JEditorPane;
-import javax.swing.JLabel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.event.HyperlinkListener;
 
 import cc.arduino.Constants;
 import processing.app.PreferencesData;
 import processing.app.Theme;
-
-import java.awt.event.KeyEvent;
 
 import static processing.app.I18n.tr;
 
@@ -67,8 +56,7 @@ public class NotificationPopup extends JDialog {
     void onOptionalButton2Callback();
   }
 
-  public NotificationPopup(Frame parent, HyperlinkListener hyperlinkListener,
-                           String message) {
+  public NotificationPopup(Frame parent, HyperlinkListener hyperlinkListener, String message) {
     this(parent, hyperlinkListener, message, true, null, null, null);
   }
 
@@ -78,14 +66,13 @@ public class NotificationPopup extends JDialog {
   }
 
   public NotificationPopup(Frame parent, HyperlinkListener hyperlinkListener,
-                           String message, boolean _autoClose, OptionalButtonCallbacks listener, String button1Name, String button2Name) {
+                           String message, boolean _autoClose, OptionalButtonCallbacks listener,
+                           String button1Name, String button2Name) {
     super(parent, false);
 
     if (!PreferencesData.getBoolean("ide.accessible")) {
-      // often auto-close is too fast for users of screen readers, so don't allow it.
       autoClose = _autoClose;
-    }
-    else {
+    } else {
       autoClose = false;
     }
     optionalButtonCallbacks = listener;
@@ -101,79 +88,30 @@ public class NotificationPopup extends JDialog {
 
     JEditorPane text = new JEditorPane();
     text.setBorder(new LineBorder(new Color(0, 0, 0), 0, true));
-    text.setContentType("text/html"); // NOI18N
+    text.setContentType("text/html");
     text.setOpaque(false);
     text.setEditable(false);
-    text.setText("<html><body style=\"font-family:sans-serif; font-size: "
-                 + scale(14) + ";\">  " + message + "  </body></html>");
+    text.setText("<html><body style=\"font-family:sans-serif; font-size: " + scale(14) + ";\">" + message + "</body></html>");
     text.addHyperlinkListener(hyperlinkListener);
     add(text);
 
     if (button1Name != null) {
       JButton optionalButton1 = new JButton(tr(button1Name));
-      MouseAdapter button1Action = new MouseAdapter() {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-          if (optionalButtonCallbacks != null) {
-            optionalButtonCallbacks.onOptionalButton1Callback();
-          }
+      optionalButton1.addActionListener(e -> {
+        if (optionalButtonCallbacks != null) {
+          optionalButtonCallbacks.onOptionalButton1Callback();
         }
-      };
-      optionalButton1.addMouseListener(button1Action);
-
-      KeyListener button1Key = new KeyListener() {
-        // Ignore when the key is typed - only act once the key is released
-        public void keyTyped(KeyEvent e) {
-          // do nothing here, wait until the key is released
-        }
-
-        // Ignore when the key is pressed - only act once the key is released
-        public void keyPressed(KeyEvent e) {
-          // do nothing here, wait until the key is released
-        }
-
-        public void keyReleased(KeyEvent e) {
-          int key = e.getKeyCode();
-          if ((key == KeyEvent.VK_ENTER) || (key == KeyEvent.VK_SPACE)) {
-            optionalButtonCallbacks.onOptionalButton1Callback();
-          }
-        }
-      };
-      optionalButton1.addKeyListener(button1Key);
+      });
       add(optionalButton1);
     }
 
     if (button2Name != null) {
       JButton optionalButton2 = new JButton(tr(button2Name));
-      MouseAdapter button2Action = new MouseAdapter() {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-          if (optionalButtonCallbacks != null) {
-            optionalButtonCallbacks.onOptionalButton2Callback();
-          }
+      optionalButton2.addActionListener(e -> {
+        if (optionalButtonCallbacks != null) {
+          optionalButtonCallbacks.onOptionalButton2Callback();
         }
-      };
-      optionalButton2.addMouseListener(button2Action);
-
-      KeyListener button2Key = new KeyListener() {
-        // Ignore when the key is typed - only act once the key is released
-        public void keyTyped(KeyEvent e) {
-          // do nothing here, wait until the key is released
-        }
-
-        // Ignore when the key is pressed - only act once the key is released
-        public void keyPressed(KeyEvent e) {
-          // do nothing here, wait until the key is released
-        }
-
-        public void keyReleased(KeyEvent e) {
-          int key = e.getKeyCode();
-          if ((key == KeyEvent.VK_ENTER) || (key == KeyEvent.VK_SPACE)) {
-            optionalButtonCallbacks.onOptionalButton2Callback();
-          }
-        }
-      };
-      optionalButton2.addKeyListener(button2Key);
+      });
       add(optionalButton2);
     }
 
@@ -181,29 +119,10 @@ public class NotificationPopup extends JDialog {
     JButton closeButton = new JButton(new ImageIcon(close));
     closeButton.setBorder(null);
     closeButton.setBorderPainted(false);
-    closeButton.setHideActionText(true);
     closeButton.setOpaque(false);
     closeButton.setBackground(new Color(0, 0, 0, 0));
     closeButton.getAccessibleContext().setAccessibleDescription(tr("Close"));
-    KeyListener closeKey = new KeyListener() {
-      // Ignore when the key is typed - only act once the key is released
-      public void keyTyped(KeyEvent e) {
-        // do nothing here, wait until the key is released
-      }
-
-      // Ignore when the key is pressed - only act once the key is released
-      public void keyPressed(KeyEvent e) {
-        // do nothing here, wait until the key is released
-      }
-
-      public void keyReleased(KeyEvent e) {
-        int key = e.getKeyCode();
-        if ((key == KeyEvent.VK_ENTER) || (key == KeyEvent.VK_SPACE)) {
-          close();
-        }
-      }
-    };
-    closeButton.addKeyListener(closeKey);
+    closeButton.addActionListener(e -> close());
     add(closeButton);
 
     MouseAdapter closeOnClick = new MouseAdapter() {
@@ -242,9 +161,8 @@ public class NotificationPopup extends JDialog {
 
   private void updateLocation(Frame parent) {
     Point parentLocation = parent.getLocation();
-
-    int parentX = Double.valueOf(parentLocation.getX()).intValue();
-    int parentY = Double.valueOf(parentLocation.getY()).intValue();
+    int parentX = (int) parentLocation.getX();
+    int parentY = (int) parentLocation.getY();
     setLocation(parentX, parentY + parent.getHeight() - getHeight());
   }
 
